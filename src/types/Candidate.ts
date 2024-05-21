2c1ae16410d962aee480d9cc621abe1b5ed5eb09
wahyu <wabredz1234@gmail.com>
2024-05-21 23:54:09 +0700

    feat : update Candidate.ts file

--- src/types/Candidate.ts
+++ src/types/Candidate.ts
@@ -5,3 +5,27 @@ export interface Candidate {
   number: number;
   batch: string;
 }
+
+export const candidates: Candidate[] = [
+  {
+    id: 1,
+    name: "Elon Musk",
+    photo: "/Candidate 1.jpg",
+    number: 1,
+    batch: "2024",
+  },
+  {
+    id: 2,
+    name: "Wahyu Jebredz",
+    photo: "/Candidate 2.jpeg",
+    number: 2,
+    batch: "2024",
+  },
+  {
+    id: 3,
+    name: "Shandika Galih",
+    photo: "/Candidate 3.jpg",
+    number: 3,
+    batch: "2024",
+  },
+];
