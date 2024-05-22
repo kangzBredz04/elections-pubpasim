98e62153fdefd474f298fa97d2cdc9a348f25d4f
wahyu <wabredz1234@gmail.com>
2024-05-22 07:31:10 +0700

    feat: Revamp application UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import CandidateCard from "./components/CandidateCard";
+import React from "react";
 import { candidates } from "./types/Candidate";
+import CandidateCard from "./components/CandidateCard";
+import Sidebar from "./components/Sidebar";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <>
-      <div className="max-w-2xl mx-auto p-4">
-        <h1 className="text-2xl font-bold mb-4">Election Candidates</h1>
-        <div className="grid grid-cols-1 gap-4">
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-grow p-4">
+        <h1 className="text-2xl font-bold mb-4">Kandidat Calon Presiden RI</h1>
+        <div className="flex flex-col gap-4 lg:gap-6 md:flex-row">
           {candidates.map((candidate) => (
             <CandidateCard key={candidate.id} candidate={candidate} />
           ))}
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default App;
