ba71b8f29c9ef0e3e44f3efbfc07a078a6632f1d
wahyu <wabredz1234@gmail.com>
2024-05-23 13:19:22 +0700

    chore: Update website title

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ const App: React.FC = () => {
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex-grow p-4">
-        <h1 className="text-2xl font-bold mb-4">Kandidat Calon Presiden RI</h1>
+        <h1 className="text-2xl font-bold mb-4">
+          Kandidat Calon Ketua PUB 2024-2025
+        </h1>
         <div className="flex flex-col gap-4 lg:gap-6 md:flex-row">
           {candidates.map((candidate) => (
             <CandidateCard key={candidate.id} candidate={candidate} />
